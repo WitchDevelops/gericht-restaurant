e6ded17da1ae91496d4e7f6c2a7c769137eaf8cc
WitchDevelops <d.n.wojewska@gmail.com>
2024-01-14 15:30:59 +0100

    feat: file structure, app backbone

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Subheading from "./Subheading/Subheading";
+import Newsletter from "./Footer/Newsletter";
+import FooterOverlay from "./Footer/FooterOverlay";
+import MenuItem from "./MenuItem/MenuItem";
+import Navbar from "./Navbar/Navbar";
+
+export { Subheading, Newsletter, FooterOverlay, MenuItem, Navbar };
---
+++ src/container/Menu/SpecialMenu.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SpecialMenu = () => {
+  return (
+    <div>Menu</div>
+  )
+}
+
+export default SpecialMenu
