cad4557cd5c1aa4e224b83fdc71a1daee8a837d1
WitchDevelops <d.n.wojewska@gmail.com>
2024-01-15 11:50:54 +0100

    feat: todays special layout styles

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import './MenuItem.css';
 
-const MenuItem = () => {
+const MenuItem = ({ title, price, tags }) => {
   return (
-    <div>Menuitem</div>
+    <div className="app__menuitem">
+      <div className="app__menuitem--head p__cormorant">
+        <p className="app__menuitem--title">{title}</p>
+        <div className="app__menuitem--line"></div>
+        <p className="app__menuitem--price">{price}</p>
+      </div>
+      <div className="app__menuitem--tags">
+        <p className="app__menuitem--tag">{tags}</p>
+      </div>
+    </div>
+
   )
 }
 
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Subheading } from '../../components';
+import { Subheading, MenuItem } from '../../components';
 import { data } from '../../constants';
 import { images } from '../../constants';
 import './SpecialMenu.css';
@@ -7,36 +7,40 @@ import './SpecialMenu.css';
 const SpecialMenu = () => {
   return (
     <section className="app__specialMenu app__wrapper section__padding">
-      <div className="app__specialMenu-title">
+
+      <div className="app__specialMenu--title">
         <Subheading title="Menu That Fits Your Palate" className="subheading" />
         <h1 className="headtext__cormorant">Today's Special</h1>
       </div>
-      <div className="app__specialMenu-menu">
-        <div className="app__specialMenu-menu_wine">
-          <p className="app__specialMenu-menu_heading">Wine & Beer</p>
-          <div className="app__specialMenu_menu_items">
+
+      <div className="app__specialMenu--menu">
+        <div className="app__specialMenu--menu_wine">
+          <p className="app__specialMenu--menu_heading">Wine & Beer</p>
+          <div className="app__specialMenu--menu_items">
             {data.wines.map((wine, index) => (
-              <p className="p__cormorant" key={index}>{wine.title}</p>
+              <MenuItem key={wine.title + index} title={wine.title} price={wine.price} tags={wine.tags}/>
             ))}
           </div>
         </div>
 
-        <div className="app__specialMenu-menu_img">
+        <div className="app__specialMenu--menu_img">
           <img src={images.menu} alt="menu img" />
         </div>
-        <div className="app__specialMenu-menu_cocktails">
-          <p className="app__specialMenu-menu_heading">Cocktails</p>
-          <div className="app__specialMenu_menu_items">
+
+        <div className="app__specialMenu--menu_cocktails">
+          <p className="app__specialMenu--menu_heading">Cocktails</p>
+          <div className="app__specialMenu--menu_items">
             {data.cocktails.map((cocktail, index) => (
-              <p className="p__cormorant" key={index}>{cocktail.title}</p>
+              <MenuItem key={cocktail.title + index} title={cocktail.title} price={cocktail.price} tags={cocktail.tags} />
             ))}
           </div>
-
         </div>
       </div>
+
       <div>
         <button className="custom__button">View More</button>
       </div>
+      
     </section>
   )
 }
