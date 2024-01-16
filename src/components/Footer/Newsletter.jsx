54a93612034a876e8056db1d12293fbb5e404ed3
WitchDevelops <d.n.wojewska@gmail.com>
2024-01-16 20:44:52 +0100

    feat: newsletter form validation

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { Subheading } from '../../components';
 import './Newsletter.css';
 
 const Newsletter = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const [submitted, setSubmitted] = useState(false);
+
+  const onSubmit = (data) => {
+    // alert(JSON.stringify(data));
+    console.log(data);
+    setSubmitted(true);
+
+  };
+
   return (
     <section className="app__newsletter section__padding">
       <div className="app__newsletter--header">
@@ -11,10 +22,40 @@ const Newsletter = () => {
         <p className="p__opensans">And never miss latest updates!</p>
       </div>
 
-      <form className="app__newsletter--form">
-        <input type="email" placeholder="Email address" className="app__newsletter--form-input"/>
-        <button type="submit" className="custom__button">Subscribe</button>
-      </form>
+      {
+        submitted ? (
+          <div className="app__newsletter--form-success">
+            <h3 className="app__newsletter--form-success-text">Thank you for subscribing!</h3>
+          </div>
+        ) : (
+          <form
+            className="app__newsletter--form"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            {errors.email &&
+              <p className="app__newsletter--form-error">{errors.email?.message}</p>
+            }
+            <input
+              placeholder="Email address"
+              className="app__newsletter--form-input"
+              name="email"
+              {...register(
+                "email",
+                {
+                  required: "Email Address is required",
+                  pattern: {
+                    value: /^\S+@\S+$/i,
+                    message: "Invalid email address",
+                  },
+                })
+              }
+            />
+            <button type="submit" className="custom__button">Subscribe</button>
+          </form>
+        )
+      }
+
+
     </section>
   )
 }
